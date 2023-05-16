be14e26bdd0fbd563116f596a71166c312fc73ba
duy1111 <maixuanduy0605@gmail.com>
2023-05-16 23:07:48 +0700

    pt3

--- src/components/Main.js
+++ src/components/Main.js
@@ -142,7 +142,7 @@ const Main = () => {
            
             <Center click={click}>
                 <YinYang  onClick={()=> handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
-                <span>click here</span>
+                <span>ấn vào đây nè!</span>
             </Center>
 
             <Contact target="_blank" href="mailto:maixuanduy06052002@gmail.com">
