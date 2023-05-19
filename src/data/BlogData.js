79801b78d5a07c05bf5cecbc048d11e7cb7b4711
duy1111 <maixuanduy0605@gmail.com>
2023-05-19 23:56:52 +0700

    upload image

--- src/data/BlogData.js
+++ src/data/BlogData.js
@@ -58,6 +58,22 @@ export const Blogs = [
         imgSrc:"https://res.cloudinary.com/dcro4uhpj/image/upload/c_thumb,w_200,g_face/v1684514849/pt/tusuong7_wtk83g.png",
         link:"https://www.facebook.com/stories/186133045877801/UzpfSVNDOjgxNzk3ODk1NjM1Nzg4NQ==/?view_single=false"
 
+    },{
+        id:8,
+        name:"chụp với bạn thân nè",
+        tags:["cute","banthan","cafe"],
+        date:"19 May, 2023",
+        imgSrc:"https://res.cloudinary.com/dcro4uhpj/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1684515283/pt/tusuong8_hicubg.png",
+        link:"https://www.instagram.com/stories/pthao.phthao/3106257494987808684/"
+
+    },{
+        id:9,
+        name:"Ngầu chưa",
+        tags:["cute","cafe","banthan"],
+        date:"19 May, 2023",
+        imgSrc:"https://res.cloudinary.com/dcro4uhpj/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1684515294/pt/tusuong9_crymxp.png",
+        link:"https://www.instagram.com/stories/pthao.phthao/3106257494987808684/"
+
     }
   
 ] 
